4987341500add503d102664bf15a5afb80251eb3
Gilles <sellig.blater@free.fr>
2023-05-21 17:24:35 +0200

    Create centered container

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <>
-      <h1>Hello world !</h1>
+      <main className="container mx-auto my-4">
+        <h1 className="text-4xl">Hello world !</h1>
+      </main>
     </>
   );
 }
